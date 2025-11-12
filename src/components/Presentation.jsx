6287710864648b7f47151b61ef8b2557d1b8aa9d
giuseppe.casaburi <giuseppe.casaburi96@gmail.com>
2025-11-12 23:56:50 +0100

    Aggiunti nuovi componenti e sezioni alla pagina Presentation; implementato il contatore per i dati aziendali; aggiornato il layout e lo stile CSS per migliorare l'estetica e la responsività.

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,4 +1,12 @@
+import Counter from "./Counter";
+
 function Presentation() {
+    const items = [
+        { target: 50, text: "Anni nel settore" },
+        { target: 100, text: "Articoli selezionati" },
+        { target: 100, simbol: "%", text: "Made in Italy" }
+    ];
+
     return (
         <>
             <section id="company-presentation">
@@ -12,6 +20,9 @@ function Presentation() {
                         <div id="titolo">
                             <h2>La nostra storia ha radici molto profonde</h2>
                         </div>
+                        <div id="image-presentation-responsive">
+                            <img src="./family.jpg" alt="" />
+                        </div>
                         <div id="testo">
                             <div className="retta"></div>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ea architecto unde labore soluta vero, itaque ab ducimus quia cupiditate veniam quisquam quasi repudiandae mollitia nihil consequatur aut blanditiis illum!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ea architecto unde labore soluta vero, itaque ab ducimus quia cupiditate veniam quisquam quasi repudiandae mollitia nihil consequatur aut blanditiis illum!</p>
@@ -22,6 +33,54 @@ function Presentation() {
                         <div id="bottone">
                             <a href="" className="button-link">Scopri di più</a>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section id="identity">
+                <div id="container-identity">
+                    <div id="content-text-area">
+                        <div id="titolo">
+                            <h2>La nostra storia ha radici molto profonde</h2>
+                        </div>
+                        <div id="testo">
+                            <div className="retta"></div>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ea architecto unde labore soluta vero, itaque ab ducimus quia cupiditate veniam quisquam quasi repudiandae mollitia nihil consequatur aut blanditiis illum!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ea architecto unde labore soluta vero, itaque ab ducimus quia cupiditate veniam quisquam quasi repudiandae mollitia nihil consequatur aut blanditiis illum!</p>
+                        </div>
+                    </div>
+                    <div id="company-number">
+                        <div id="conteiner-company-number">
+                            <div className="col">
+                                <div className="text-col">
+                                    <p>100%</p>
+                                    <span>Made <br />in Italy</span>
+                                </div>
+                                <div className="text-col">
+                                    <p>100+</p>
+                                    <span>Articoli selezionati</span>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="text-col">
+                                    <p>100%</p>
+                                    <span>Clienti soddisfatti</span>
+                                </div>
+                                <div className="text-col">
+                                    <p>50+</p>
+                                    <span>Anni nel settore</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="company-banner">
+                <div id="conteiner-company-banner">
+                    <div id="overlay">
+                    </div>
+                    <div id="company-banner-content">
+                        <h2>Affidati a dei professionisti</h2>
+                        <p>Contattaci per una consulenza gratuita</p>
+                        <a className="button-link">Contatti</a>
                     </div>
                 </div>
             </section>
